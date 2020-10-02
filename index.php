c268f6c7083ce0b754cb835508329f06a18d428c
lespaul98xd <jdgarias98@gmail.com>
2020-10-02 13:45:31 -0500

    se actualiza el formulario
    form-text text-muted

--- index.php
+++ index.php
@@ -11,52 +11,53 @@
     <hr>
     </header>
 
-    <form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4">
-    </div>
-    <div class="form-group col-md-6">
-      <label for="inputPassword4">Password</label>
-      <input type="password" class="form-control" id="inputPassword4">
-    </div>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                <form action="">
+
+                <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Cedula De Ciudadania</label>
+    <input type="text" class="form-control" >
+    <small  class="form-text text-muted">Ingresa por favor tu cedula de ciudadania.</small>
   </div>
   <div class="form-group">
-    <label for="inputAddress">Address</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+    <label for="exampleInputPassword1">Nombre</label>
+    <input type="text" class="form-control" >
+    <small  class="form-text text-muted">Ingresa por favor tu Nombre.</small>
   </div>
-  <div class="form-group">
-    <label for="inputAddress2">Address 2</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-  </div>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputCity">City</label>
-      <input type="text" class="form-control" id="inputCity">
-    </div>
-    <div class="form-group col-md-4">
-      <label for="inputState">State</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-    <div class="form-group col-md-2">
-      <label for="inputZip">Zip</label>
-      <input type="text" class="form-control" id="inputZip">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck">
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
+ 
 </form>
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Apellidos</label>
+    <input type="text" class="form-control" >
+    <small  class="form-text text-muted">Ingresa por favor tus Apellidos.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Edad</label>
+    <input type="text" class="form-control" >
+    <small  class="form-text text-muted">Ingresa por favor tu Edad.</small>
+  </div>
+ 
+</form>
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Telefono</label>
+    <input type="text" class="form-control" >
+    <small  class="form-text text-muted">Ingresa por favor tu Telefono.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Dirección</label>
+    <input type="text" class="form-control" >
+    <small  class="form-text text-muted">Ingresa por favor tu Dirección.</small>
+  </div>
+ 
+</form>
+
+    </main>
 
     <footer></footer>
 
