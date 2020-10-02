d904d1c41cda1a92d68e7a81dc4164a1e494535b
lespaul98xd <jdgarias98@gmail.com>
2020-10-02 14:49:49 -0500

    ultima actualizacion viernes

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
 
     <main>
         <div class="container">
-            <div class="row">
+            <div class="row justify-content-center">
                 <div class="col-4">
                 <form action="">
 
                 <form>
   <div class="form-group">
     <label for="exampleInputEmail1">Cedula De Ciudadania</label>
-    <input type="text" class="form-control" >
+    <input type="number" class="form-control" >
     <small  class="form-text text-muted">Ingresa por favor tu cedula de ciudadania.</small>
   </div>
   <div class="form-group">
@@ -46,7 +46,7 @@
 <form>
   <div class="form-group">
     <label for="exampleInputEmail1">Telefono</label>
-    <input type="text" class="form-control" >
+    <input type="number" class="form-control" >
     <small  class="form-text text-muted">Ingresa por favor tu Telefono.</small>
   </div>
   <div class="form-group">
@@ -54,15 +54,12 @@
     <input type="text" class="form-control" >
     <small  class="form-text text-muted">Ingresa por favor tu Dirección.</small>
   </div>
- 
+  <button type="submit" class="btn btn-primary btn-block">Registrar</button>
 </form>
 
     </main>
 
     <footer></footer>
-
-
-
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
@@ -71,3 +68,9 @@
 
 </body>
 </html>
+
+
+<!--
+  esto es un formulario basico en bootstrap4
+  -->
+
